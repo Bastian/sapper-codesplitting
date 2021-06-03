ae75f2c682bec79eff792fa4aa4526ce0b21df7f
Bastian Oppermann <bastianoppermann1997@gmail.com>
2021-06-03 13:20:01 +0200

    Add moment to about page

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,7 +1,15 @@
+<script>
+  import moment from "moment";
+
+  const time = moment().format();
+</script>
+
 <svelte:head>
-	<title>About</title>
+  <title>About</title>
 </svelte:head>
 
 <h1>About this site</h1>
 
 <p>This is the 'about' page. There's not much here.</p>
+
+<p>It's {time}</p>
